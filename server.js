70dae6d4a7b2fb691db27a3ec4a335b9cff1a80b
Manuel Henriquez <manuelhm1993@gmail.com>
2023-04-17 16:44:22 -0400

    Configuracion de servidor
    Se crearon las configuraciones de conexión a la base de datos

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+// Módulos                 ---------------------------------------------------------
 const express = require('express')
-
 const app = express()
+const mysql = require('mysql')
+const conexion = require('express-myconnection')
 
+// Configuraciones        ---------------------------------------------------------
 app.set('port', process.env.PORT || 9000)
 
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'pruebabackend'
+}
+
+// Middlewares            ---------------------------------------------------------
+app.arguments(conexion(mysql, dbOptions, 'single'))
+
+// Rutas                  ---------------------------------------------------------
 app.get('/', (req, res) => {
     res.send('Bienvenido a mi API')
 })
 
+// Ejecución del servidor ---------------------------------------------------------
 app.listen(app.get('port'), () => {
     console.log('El servidor está corriendo en el puerto', app.get('port'))
 })
